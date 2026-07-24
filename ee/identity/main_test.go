2798bdfaf4a83cb6cab4bc7a71cf0d8bcf046bfd
Axel Marciano <axel.marciano@gmail.com>
2026-07-24 14:28:16 +0200

    feat: ajouter des tests unitaires pour les packages audit, identity et observe

---
+++ ee/identity/main_test.go
@@ -0,0 +1,18 @@
+// Copyright (c) 2026 Axel Marciano (Mercure Technologies). All rights reserved.
+// This file is governed by the Mercure Technologies Enterprise Edition License
+// (see ee/LICENSE); it is NOT covered by the MIT license of this repository.
+
+package identity
+
+import (
+	"os"
+	"testing"
+
+	"expo-open-ota/internal/database/postgres/pgtest"
+)
+
+// The Postgres-backed tests share TEST_DATABASE_URL with other packages;
+// pgtest serializes them so migrations and test transactions cannot conflict.
+func TestMain(m *testing.M) {
+	os.Exit(pgtest.RunSerialized(m))
+}
